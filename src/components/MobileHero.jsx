a4279e9d33f65f117fcc40ed80101ae63a97375c
MOYIN-21 <olawamidemoyinoluwamary@gmail.com>
2024-07-11 10:44:46 +0100

    boredr

--- src/components/MobileHero.jsx
+++ src/components/MobileHero.jsx
@@ -6,10 +6,10 @@ import SW from "../assets/new-removebg-preview.png";
 const MobileHero = () => {
     return (
         <div className="bg-cover bg-center relative min-h-screen block lg:hidden" style={{ backgroundImage: `url(${bg})` }}>
-            <div className="absolute inset-0 z-10 flex flex-col items-center">
+            <div className="absolute inset-0 z-10 flex flex-col justfy-center center items-center">
                 <div className="mb-44 text-center">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-blue-900">WILLFEAD WATER</h2>
-                    <p className="text-lg md:text-xl tracking-tight font-extrabold text-blue-900">
+                    <p className="text-lg md:text-xl tracking-tight font-extrabold text-blue-900 px-3">
                         Water is life's matter and matrix, mother and medium <br />
                         There is no life without water
                     </p>
